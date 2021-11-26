7c8b91a454529e32a9899d0c3c726b185bb3d42d
Rei Hashimoto <reihashimoto@ReinoMacBook-Pro.local>
2021-11-27 02:08:04 +0900

    first commit

---
+++ MyJanken2/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by Rei Hashimoto on 2021/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var answerNumber = 0
+    
+    var body: some View {
+        VStack{
+            if answerNumber == 0 {
+                Text("これからじゃんけんをします")
+                    .padding(.bottom)
+            } else if answerNumber == 1 {
+                Image("gu")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+                 Text("グー")
+                Spacer()
+                    .padding(.bottom)
+            } else if answerNumber == 2 {
+                Image("choki")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+                 Text("チョキ")
+                Spacer()
+                padding(.bottom)
+            } else {
+                Image("pa")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+                 Text("パー")
+                Spacer()
+                padding(.bottom)
+            }
+       
+        VStack {
+             
+            Spacer()
+            
+            Button(action: {
+               var newAnswerNumber = 0
+                repeat {
+                    newAnswerNumber = Int.random(in: 1...3)
+                } while answerNumber == newAnswerNumber
+                answerNumber = newAnswerNumber
+            }) {
+                Text("じゃんけんをする！")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
+            }
+        }
+        
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+}
---
+++ MyJanken2/MyJanken2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyJanken2App.swift
+//  MyJanken2
+//
+//  Created by Rei Hashimoto on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct MyJanken2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
